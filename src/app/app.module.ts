fc5c6ffa7a520bfbb86fcdefc5c7a7b7b6156843
binhnguyen95 <invalidmyarse@gmail.com>
2022-11-02 17:26:13 +0700

    create model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JobListComponent } from './user/job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule
