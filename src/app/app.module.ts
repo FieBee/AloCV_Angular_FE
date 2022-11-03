83cc8d094971f3662c82f2ddd1adbefe48b22561
binhnguyen95 <invalidmyarse@gmail.com>
2022-11-03 10:50:02 +0700

    fix model, add company-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+// import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import {JobListComponent} from "./user/job-list/job-list.component";
+import { CompanyListComponent } from './company/company-list/company-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    // HeaderComponent,
     FooterComponent,
-    JobListComponent
+    JobListComponent,
+    CompanyListComponent
   ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,74 +1,74 @@
-import {Component, OnInit} from '@angular/core';
-import {TokenService} from '../security/token.service';
-import {CompanyService} from '../service/company/company.service';
-import {Company} from '../model/company';
-import {Router} from '@angular/router';
-import {User} from '../model/user';
-import {UserService} from '../user/service/user.service';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderComponent implements OnInit {
-  checkLogin = false;
-
-  checkRole: string;
-
-  // @ts-ignore
-  name: string;
-
-  // @ts-ignore
-  idGuest: number;
-
-  // @ts-ignore
-  company: Company;
-
-  user: User;
-
-  constructor(private tokenService: TokenService,
-              private companyService: CompanyService,
-              private userService: UserService,
-              private router: Router) {
-  }
-
-  ngOnInit(): void {
-    if (this.tokenService.getTokenKey()) {
-      this.checkLogin = true;
-      this.idGuest = this.tokenService.getIdGuest();
-      if (this.idGuest == -10) {
-        this.checkRole = 'ADMIN';
-        this.name = "ADMIN"
-      }
-      else {
-        for (let i = 0; i < this.tokenService.getRoleKey().length; i++) {
-          if (this.tokenService.getRoleKey()[i] == 'COMPANY') {
-            this.companyService.getCompanyNameById(this.idGuest).subscribe(data => {
-              console.log(data);
-              this.company = data;
-              this.checkRole = 'COMPANY';
-              this.name = this.company.name;
-            });
-          }
-          if (this.tokenService.getRoleKey()[i] == 'USER') {
-            this.userService.getUserById(this.idGuest).subscribe(data => {
-              console.log(data);
-              this.user = data;
-              this.checkRole = 'USER';
-              this.name = this.user.name;
-            });
-          }
-        }
-      }
-
-    }
-  }
-
-  logOut() {
-    window.sessionStorage.clear();
-    this.router.navigate(['login']).then(() => {
-      window.location.reload();
-    });
-  }
-}
+// import {Component, OnInit} from '@angular/core';
+// // import {TokenService} from '../security/token.service';
+// import {CompanyService} from '../service/company/company.service';
+// import {Company} from '../model/company';
+// import {Router} from '@angular/router';
+// import {User} from '../model/user';
+// // import {UserService} from '../user/service/user.service';
+//
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.scss']
+// })
+// export class HeaderComponent implements OnInit {
+//   checkLogin = false;
+//
+//   checkRole: string | undefined;
+//
+//   // @ts-ignore
+//   name: string;
+//
+//   // @ts-ignore
+//   idGuest: number;
+//
+//   // @ts-ignore
+//   company: Company;
+//
+//   user: User;
+//
+//   constructor(private tokenService: TokenService,
+//               private companyService: CompanyService,
+//               private userService: UserService,
+//               private router: Router) {
+//   }
+//
+//   ngOnInit(): void {
+//     if (this.tokenService.getTokenKey()) {
+//       this.checkLogin = true;
+//       this.idGuest = this.tokenService.getIdGuest();
+//       if (this.idGuest == -10) {
+//         this.checkRole = 'ADMIN';
+//         this.name = "ADMIN"
+//       }
+//       else {
+//         for (let i = 0; i < this.tokenService.getRoleKey().length; i++) {
+//           if (this.tokenService.getRoleKey()[i] == 'COMPANY') {
+//             this.companyService.getCompanyNameById(this.idGuest).subscribe(data => {
+//               console.log(data);
+//               this.company = data;
+//               this.checkRole = 'COMPANY';
+//               this.name = this.company.name;
+//             });
+//           }
+//           if (this.tokenService.getRoleKey()[i] == 'USER') {
+//             this.userService.getUserById(this.idGuest).subscribe(data => {
+//               console.log(data);
+//               this.user = data;
+//               this.checkRole = 'USER';
+//               this.name = this.user.name;
+//             });
+//           }
+//         }
+//       }
+//
+//     }
+//   }
+//
+//   logOut() {
+//     window.sessionStorage.clear();
+//     this.router.navigate(['login']).then(() => {
+//       window.location.reload();
+//     });
+//   }
+// }
