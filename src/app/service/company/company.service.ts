fe35ea8653a14ceccd8d99390a3b4b537c5ff49f
binhnguyen95 <invalidmyarse@gmail.com>
2022-11-03 11:03:49 +0700

    fix service company

--- src/app/service/company/company.service.ts
+++ src/app/service/company/company.service.ts
@@ -8,28 +8,29 @@ const API_URL = `${environment.apiUrl}`;
 @Injectable({
   providedIn: 'root'
 })
+
 export class CompanyService {
 
   constructor(private httpClient: HttpClient ) { }
 
   getAll():Observable<Company[]>{
-    return this.httpClient.get<Company[]>(`API_URL/company`)
+    return this.httpClient.get<Company[]>(API_URL)
   }
 
-  saveCompany(company:Company):Observable<Company>{
-    return this.httpClient.post<Company>(`API_URL/company`, company);
+  saveCompany(company: Company):Observable<Company>{
+    return this.httpClient.post<Company>(API_URL + `/company` , company);
   }
 
   findById(id: number):Observable<Company>{
-    return this.httpClient.get<Company>(`API_URL/company`+`/${id}`);
+    return this.httpClient.get<Company>(API_URL + `/company/${id}`);
   }
 
   editCompany(id: number | undefined, company: Company): Observable<Company>{
-    return this.httpClient.put<Company>(API_URL+`/${id}`,company);
+    return this.httpClient.put<Company>(API_URL + `/company/${id}`,company);
   }
 
-  delete(id:any): Observable<Company>{
-    return this.httpClient.delete<Company>(API_URL+`/${id}`);
+  delete(id: any): Observable<Company>{
+    return this.httpClient.delete<Company>(API_URL + `/company/${id}`);
   }
 
 }
