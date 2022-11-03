be35d03c7581e4819ccec998a919ec008489f787
FieBee <phuonglf98@gmail.com>
2022-11-03 17:29:39 +0700

    add backgroun + form register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./security/login/login.component";
 import {RegisterComponent} from "./security/register/register.component";
+import {HomeComponent} from "./home/home.component";
 
 export const ROUTES: Routes = [
   {
@@ -9,6 +10,9 @@ export const ROUTES: Routes = [
   },
   {
     path: 'register', component: RegisterComponent
+  },
+  {
+    path: '', component: HomeComponent
   }
   // { path: '**', component: NotfoundComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
 import {environment} from "../environments/environment";
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import {FooterComponent} from "./footer/footer.component";
     UploadFileComponent,
     UploadImageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
