603acca4a3553ff61fc314b69b90e940362f1689
FieBee <phuonglf98@gmail.com>
2022-11-04 12:46:07 +0700

    add submitFile() by user-register.component

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {finalize} from "rxjs";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
   selector: 'app-user-register',
@@ -7,13 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegisterComponent implements OnInit {
   userForm: any;
+  private selectedImage: any;
+  private avatarDom: any;
+  arrayPicture='';
 
-  constructor() { }
+
+  constructor(
+              private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
 
   submit() {
-    
+
+  }
+
+  uploadFileImg(){
+    this.selectedImage = this.avatarDom?.nativeElement.files[0];
+    console.log(this.selectedImage);
+    this.submitFile();
+  }
+
+
+  submitFile(){
+    if (this.selectedImage != null) {
+      const filePath = this.selectedImage.name;
+      const fileRef = this.storage.ref(filePath);
+      console.log("fp", filePath)
+      console.log("fr", fileRef)
+      this.storage.upload(filePath, this.selectedImage).snapshotChanges().pipe(
+        finalize(()=>(fileRef.getDownloadURL().subscribe(url=>{
+          this.arrayPicture = url;
+          console.log(url)
+        })))
+      ).subscribe();
+    }
   }
 }
