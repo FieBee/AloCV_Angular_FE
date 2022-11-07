86fe70cfe7341efbe527d12d130aeb87386a1ba4
binhnguyen95 <invalidmyarse@gmail.com>
2022-11-07 17:46:43 +0700

    job delete

---
+++ src/app/job/job-delete/job-delete.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import {Subscription} from "rxjs";
+import {Job} from "../../model/job";
+import {Location} from "../../model/location";
+import {JobService} from "../../service/job/job.service";
+import {LocationService} from "../../service/location/location.service";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-job-delete',
+  templateUrl: './job-delete.component.html',
+  styleUrls: ['./job-delete.component.css']
+})
+export class JobDeleteComponent implements OnInit {
+
+  sub:Subscription;
+
+  // @ts-ignore
+  job: Job = {
+    id: 0,
+    name: "",
+    // jobField: {
+    //   name: "",
+    // },
+    salaryRange: 0,
+    location : {
+      name: "",
+    },
+    position: "",
+    experience: "",
+    jobType: "",
+    // expiredDate: Date,
+    recruitNumber: 0,
+    gender: "",
+    description: "Mo ta",
+  };
+
+  id: number | undefined;
+
+  locationList: Location[] | undefined;
+
+  constructor(private jobService: JobService,
+              private locationService: LocationService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    this.sub = this.activatedRoute.paramMap.subscribe( (paramMap: ParamMap) => {
+      // @ts-ignore
+      this.id = +paramMap.get('id');
+      this.getJob(this.id);
+    })
+  }
+
+  getJob(id: number){
+    this.jobService.findById(id).
+    subscribe(job =>{
+      this.job = job;
+    });
+  }
+
+  deleteJob(id: number | undefined) {
+    this.jobService.delete(id).subscribe(() => {
+      alert('Delete success!');
+      this.router.navigate(['/job/job-list']);
+    }, e => {
+      console.log(e);
+    });
+  }
+
+  ngOnInit(){
+    this.getAllLocation();
+  }
+
+  getAllLocation() {
+    this.locationService.getAll().subscribe((result: any) => {
+      this.locationList = result;
+      console.log(result);
+    }, (error: any) => {
+      console.log(error);
+    })
+  }
+}
