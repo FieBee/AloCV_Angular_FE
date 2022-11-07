6d0998237d820dd730066f76c2a0639d74b42ef8
AnhDung95 <nguyendung2172013@gmail.com>
2022-11-07 17:04:18 +0700

    dang lam find

--- src/app/user/find-job-user/find-job-user.component.ts
+++ src/app/user/find-job-user/find-job-user.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {FindJobService} from "../../service/find-job.service";
 import {JobFieldService} from "../../service/jobField/job-field.service";
 import {JobField} from "../../model/job-field";
+import {LocationService} from "../../service/location/location.service";
+import {Location} from "../../model/location";
+import {CompanyService} from "../../service/company/company.service";
+import {Company} from "../../model/company";
+import {JobService} from "../../service/job/job.service";
+import {Job} from "../../model/job";
 
 @Component({
   selector: 'app-find-job-user',
@@ -9,21 +15,34 @@ import {JobField} from "../../model/job-field";
   styleUrls: ['./find-job-user.component.css']
 })
 export class FindJobUserComponent implements OnInit {
-  listJob: any;
+  salaryRange: number[] = [
+    1000000,
+    2000000,
+    3000000
+  ]
+
+
   public name = '';
-  public salaryRange = '';
+  // public salaryRange = '';
   public jobField = '';
   public location = '';
   public company = '';
   jobFieldList: JobField[]=[]
-
+  locationList: Location[]=[]
+  companyList: Company[]=[]
+  jobList: Job[]=[]
 
   constructor(private FindJobService: FindJobService,
-              private JobFieldService: JobFieldService) { }
+              private JobFieldService: JobFieldService,
+              private LocationService: LocationService,
+              private CompanyService: CompanyService,
+              private JobService: JobService) { }
 
   ngOnInit(): void {
     this.getAllJobField()
-    // this.searchJob(0);
+    this.getAllLocation()
+    this.getAllCompany()
+    this.getAllJob()
   }
   getSearch(pageable: any) {
     this.FindJobService.getAllJobBy(this.name, this.salaryRange, this.jobField, this.location, this.company, pageable).subscribe(data => {
@@ -31,10 +50,19 @@ export class FindJobUserComponent implements OnInit {
         console.log("Thông tin bạn tìm kiếm hiện không có trong hệ thống ", 'Thông báo !')
         // this.getListPeriodicPatient(0);
       } else {
-        this.listJob = data;
+        this.jobList = data;
       }
     });
   }
+  getAllJob() {
+    this.JobService.getAll().subscribe((result: any) => {
+      this.jobList = result;
+      console.log(result);
+    }, (error: any) => {
+      console.log(error);
+    })
+  }
+
   getAllJobField() {
     this.JobFieldService.getAll().subscribe((result: any) => {
       this.jobFieldList = result;
@@ -43,6 +71,36 @@ export class FindJobUserComponent implements OnInit {
       console.log(error);
     })
   }
+  getAllLocation() {
+    this.LocationService.getAll().subscribe((result: any) => {
+      this.locationList = result;
+      console.log(result);
+    }, (error: any) => {
+      console.log(error);
+    })
+  }
+  getAllCompany() {
+    this.CompanyService.getAll().subscribe((result: any) => {
+      this.companyList = result;
+      console.log(result);
+    }, (error: any) => {
+      console.log(error);
+    })
+  }
+  formatLabel(value: number) {
+    if (value >= 1000000) {
+      return Math.round(value / 1000000) + 'tr';
+    }
+    return value;
+  }
+  getCode(id: number, size: number): string {
+    let num = id.toString();
+    while (num.length < size) {
+      num = '0' + num;
+    }
+    return 'BN-' + num;
+  }
+
   // getListPeriodicPatient(pageable: any) {
   //   this.name = '';
   //   this.salaryRange = '';
