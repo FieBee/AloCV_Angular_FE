ee0bf85b07d47e4d811540709babf985c669a64f
FieBee <phuonglf98@gmail.com>
2022-11-10 20:47:59 +0700

    add router listCv, createCv, edit home.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,8 @@ import {JobCreateComponent} from "./job/job-create/job-create.component";
 import {JobEditComponent} from "./job/job-edit/job-edit.component";
 import {JobDeleteComponent} from "./job/job-delete/job-delete.component";
 import {FindJobUserComponent} from "./user/find-job-user/find-job-user.component";
+import {CvCreateComponent} from "./cv/cv-create/cv-create.component";
+import {CvListComponent} from "./cv/cv-list/cv-list.component";
 
 
 export const ROUTES: Routes = [
@@ -42,7 +44,7 @@ export const ROUTES: Routes = [
     path: 'user/job-list', component: UserJobListComponent
   },
   {
-    path: 'job/job-list', component: JobListComponent
+    path: '../home', component: JobListComponent
   },
   {
     path: 'job/job-create', component: JobCreateComponent
@@ -51,9 +53,13 @@ export const ROUTES: Routes = [
     path: 'job/job-edit/:id', component: JobEditComponent
   },
   {
-
     path: 'job/job-delete/:id', component: JobDeleteComponent
-
+  },
+  {
+    path: 'user/create-cv', component: CvCreateComponent
+  },
+  {
+    path: 'user/list-cv', component: CvListComponent
   },
 
 
