de0ef2d0c25737c147e142b9a6770e3eabc87de9
FieBee <phuonglf98@gmail.com>
2022-11-15 08:44:34 +0700

    add user/user-account-management c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { ApplyListComponent } from './cv/apply-list/apply-list.component';
 import { CompanyTopRecruitmentComponent } from './company/company-top-recruitment/company-top-recruitment.component';
 import { CompanyDetailComponent } from './company/company-detail/company-detail.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserAccountManagementComponent } from './user/user-account-management/user-account-management.component';
 
 
 
@@ -72,7 +73,8 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
     CompanyEditComponent,
     CompanyTopRecruitmentComponent,
     CompanyDetailComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    UserAccountManagementComponent
 
   ],
   imports: [
---
+++ src/app/user/user-account-management/user-account-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-management',
+  templateUrl: './user-account-management.component.html',
+  styleUrls: ['./user-account-management.component.css']
+})
+export class UserAccountManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
